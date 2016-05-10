dca8afa5e689dbb96649884bfb0deab9b89055f0
Likai Ding <likaid@codeaurora.org>
2016-05-10 15:09:40 +0800

    Gallery: don't flash title on launch
    Request no title feature in app theme instead of during onCreate().
    
    CRs-Fixed: 1024516
    Change-Id: I2805f1e5bdfbeaf3b4e142d9aab18b8e75669e68

--- src/com/android/gallery3d/app/GalleryActivity.java
+++ src/com/android/gallery3d/app/GalleryActivity.java
@@ -112,7 +112,6 @@ public final class GalleryActivity extends AbstractGalleryActivity implements On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         if (getIntent().getBooleanExtra(KEY_DISMISS_KEYGUARD, false)) {
             getWindow().addFlags(
