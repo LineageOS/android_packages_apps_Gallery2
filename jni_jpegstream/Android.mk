348658059575aa7a3bd722a1f9b06d0683225e9c
David Ng <dave@codeaurora.org>
2016-12-09 00:07:42 -0800

    Switch to static NDK jpeg library
    Remove dependency on platform jpeg shared library.
    
    Change-Id: Ib4ec3d83a5e4a609c8ddf4609e0699aa43203097

--- jni_jpegstream/Android.mk
+++ jni_jpegstream/Android.mk
@@ -12,9 +12,9 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/src \
                     external/jpeg
 
-LOCAL_SHARED_LIBRARIES := libjpeg
+LOCAL_STATIC_LIBRARIES := libjpeg_static_ndk
 
-LOCAL_SDK_VERSION   := 9
+LOCAL_SDK_VERSION   := 17
 LOCAL_ARM_MODE := arm
 
 LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops
