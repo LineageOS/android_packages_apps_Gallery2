73095869bd60577f49b6cc3423f59e6513932da9
Likai Ding <likaid@codeaurora.org>
2017-04-20 15:02:08 +0800

    Gallery: fix crash when saving image
    Default filter doesn't have serialization name. Check before writing.
    
    Change-Id: Ide99e60b6de9f123ad51710a1b2ed53805ff02cd
    CRs-Fixed: 2017800

--- src/com/android/gallery3d/filtershow/pipeline/ImagePreset.java
+++ src/com/android/gallery3d/filtershow/pipeline/ImagePreset.java
@@ -788,6 +788,9 @@ public class ImagePreset {
                     continue;
                 }
                 String sname = filter.getSerializationName();
+                if (sname == null) {
+                    continue;
+                }
                 if (DEBUG) {
                     Log.v(LOGTAG, "Serialization: " + sname);
                     if (sname == null) {
