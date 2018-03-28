9859449cba8536e3c24f9a1103307c879aba2e7b
Aurimas Liutikas <aurimas@google.com>
2018-03-28 11:45:44 -0700

    Move Gallery2 to androidx.
    Bug: 76692459
    Test: make -j Gallery2 and run on emulator
    Change-Id: Ife6a5649c21464f404db3198bd6a4ba8aa901ee8

--- src/com/android/gallery3d/ui/BaseDialogFragment.java
+++ src/com/android/gallery3d/ui/BaseDialogFragment.java
@@ -28,9 +28,9 @@
  */
 package com.android.gallery3d.ui;
 
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class BaseDialogFragment extends DialogFragment {
 
