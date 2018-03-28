fdc5b4dd969bc836e25fe7eef73130f488db927d
Aurimas Liutikas <aurimas@google.com>
2018-03-28 11:45:44 -0700

    Move Gallery2 to androidx.
    Bug: 76692459
    Test: make -j Gallery2 and run on emulator
    Change-Id: Ife6a5649c21464f404db3198bd6a4ba8aa901ee8

--- src/com/android/gallery3d/filtershow/category/BottomPanel.java
+++ src/com/android/gallery3d/filtershow/category/BottomPanel.java
@@ -30,7 +30,7 @@
 package com.android.gallery3d.filtershow.category;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
