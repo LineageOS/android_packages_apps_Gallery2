25ad772889e3ac164b236b0e44928b6d84dc2347
Michael Bestas <mkbestas@lineageos.org>
2020-05-06 21:01:08 +0300

    Gallery2: Add privapp permissions xml
    Change-Id: I269a237f17ecaccfbcf6b0f1017253278f69c9a1

--- Android.mk
+++ Android.mk
@@ -59,7 +59,18 @@ LOCAL_JAVA_LIBRARIES += org.apache.http.legacy
 
 LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
 
+LOCAL_REQUIRED_MODULES += privapp_whitelist_com.android.gallery3d.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.android.gallery3d.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_PRODUCT_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 ifeq ($(strip $(LOCAL_PACKAGE_OVERRIDES)),)
 
