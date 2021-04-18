6592238ec8a7695090bdc810ed5899d318319a64
Wanja Schulze <wangenau@protonmail.com>
2021-04-18 16:05:33 +0200

    Gallery2: Fix black on black text
    Change-Id: I41cc62bcea0210080ee1e231e1c4b9a9f62a93a0

--- src/com/android/gallery3d/app/AlbumSetPage.java
+++ src/com/android/gallery3d/app/AlbumSetPage.java
@@ -258,7 +258,7 @@ public class AlbumSetPage extends ActivityState implements
         if (tvEmptyAlbum == null) {
             tvEmptyAlbum = new TextView(mActivity);
             tvEmptyAlbum.setText(R.string.tvEmptyAlbum);
-            tvEmptyAlbum.setTextColor(Color.parseColor("#8A000000"));
+            tvEmptyAlbum.setTextColor(mActivity.getResources().getColor(R.color.empty_text));
             tvEmptyAlbum.setGravity(Gravity.CENTER);
             tvEmptyAlbum.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
             tvEmptyAlbum.setId(R.id.empty_album);
