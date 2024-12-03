fcef96821aa17ae630537b803349a6147e712cdd
Cole Faust <colefaust@google.com>
2024-12-02 17:26:43 -0800

    Fix DuplicateBranches errorprone issue
    Bug: 253827323
    Test: m RUN_ERROR_PRONE=true javac-check
    Flag: EXEMPT no-op change
    Change-Id: I575c76d0a1854f158fb62c073c6c7dce3d5f699f

--- gallerycommon/src/com/android/gallery3d/exif/ExifParser.java
+++ gallerycommon/src/com/android/gallery3d/exif/ExifParser.java
@@ -623,11 +623,7 @@ class ExifParser {
             if (isThumbnailRequested()) {
                 if (tag.hasValue()) {
                     for (int i = 0; i < tag.getComponentCount(); i++) {
-                        if (tag.getDataType() == ExifTag.TYPE_UNSIGNED_SHORT) {
-                            registerUncompressedStrip(i, tag.getValueAt(i));
-                        } else {
-                            registerUncompressedStrip(i, tag.getValueAt(i));
-                        }
+                        registerUncompressedStrip(i, tag.getValueAt(i));
                     }
                 } else {
                     mCorrespondingEvent.put(tag.getOffset(), new ExifTagEvent(tag, false));
